abe8a99c29c529adda100b809b8497ea758c1445
lelis <lelis685@gmail.com>
2022-12-23 00:45:13 -0300

    fix: ajustando configuracoes

--- src/main/java/br/com/cliente/entrypoint/controller/response/AddressResponse.java
+++ src/main/java/br/com/cliente/entrypoint/controller/response/AddressResponse.java
@@ -1,13 +1,16 @@
 package br.com.cliente.entrypoint.controller.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressResponse {
 
     private String street;
     private String city;
     private String state;
-
 
 }
